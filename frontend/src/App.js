270a67ceead3272171aecf3749823e901b2ca09c
arlecchi <hasyaamalya@gmail.com>
2025-03-19 06:07:48 +0700

    navbar completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import HomePage from './component/HomePage.js';
+import'./App.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const App = ()=>{
   return(
--- frontend/src/component/Navigation.js
+++ frontend/src/component/Navigation.js
@@ -2,35 +2,43 @@
 const Navigation = ()=>{
     return(
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <div className="container">
+                <a className="navbar-brand" href="#">MerchVerse</a>
+                <li className="ms-auto me-2 d-flex align-items-center d-lg-none">
+                    <button className="btnIcon mx-2">
+                        <i class="bi bi-bag position-relative">
+                            <div className="circleBadge position-absolute top-0 end-0 transform-translate-middle"></div>
+                        </i>
+                    </button>
                 </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">Features</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
-                </li>
-                <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown link
-                </a>
-                <ul className="dropdown-menu">
-                    <li><a className="dropdown-item" href="#">Action</a></li>
-                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav mx-auto">
+                    <li className="nav-item">
+                    <a className=" nav-link mx-2 active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className=" nav-link mx-2" href="#">Category</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className=" nav-link mx-2" href="#">Product</a>
+                    </li>
+                    <li className="d-lg-flex align-items-center d-none">
+                        <button className="btnIcon mx-2">
+                            <i class="bi bi-bag position-relative">
+                                <div className="circleBadge position-absolute top-0 end-0 transform-translate-middle"></div>
+                            </i>
+                        </button>
+                    </li>
                 </ul>
-                </li>
-            </ul>
+                <div className="d-flex btnWrapper mt-lg-0 mt-5">
+                    <button className="w-100 secondaryBtn" >Sign In</button>
+                    <button className="w-100 primaryBtn">Login</button>
+                </div>
+                </div>
             </div>
-        </div>
         </nav>
     )
 }
