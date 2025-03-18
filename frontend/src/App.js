39b6eb37e8dbd04b9eb673941755567983f48eda
arlecchi <hasyaamalya@gmail.com>
2025-03-18 15:53:04 +0700

    create navbar
    minute 13:19 of episode 8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,9 @@
-import {Provider} from './component/MyContext.js';
 import HomePage from './component/HomePage.js';
-import CategoryCard from './component/CategoryCard.js';
-import BannerPromotion from './component/Banner.js';
 
 const App = ()=>{
   return(
     <>
-      <Provider>
-        <HomePage />
-        <CategoryCard/>
-        <BannerPromotion/>
-      </Provider>
-      <h1>Hello world</h1>
+      <HomePage />
     </>
   )
 }
--- frontend/src/component/HomePage.js
+++ frontend/src/component/HomePage.js
@@ -1,16 +1,9 @@
-import { useContext } from "react";
-import {Context} from './MyContext.js'
+import Navigation from "./Navigation";
 
 const HomePage = ()=>{
-    const {product} = useContext(Context)
     return(
         <>
-            <h1> this is the Homepage </h1>
-            {product.map((p)=>{
-                return(
-                    <p key={p.id}>{p.series_title}</p>
-                )
-            })}
+            <Navigation/>
         </>
     )
 }
