a6585c44988197f672cca212aa4428251c841b24
arlecchi <hasyaamalya@gmail.com>
2025-03-19 07:43:11 +0700

    create category section

--- frontend/src/component/CategoryCard.js
+++ frontend/src/component/CategoryCard.js
@@ -4,14 +4,28 @@ import { useContext } from "react"
 const CategoryCard= ()=>{
     const{category} = useContext(Context)
     return(
-        <>
-            <h1>Category Page</h1>
-            {category.map((c)=>{
-                return(
-                    <p key={c.id}>{c.description}</p>
-                )
-            })}
-        </>
+        <div className="container category my-5">
+            <div className="title-wrapper">
+                <h6>Product Category</h6>
+            </div>
+
+            <div className="row my-4">
+                {category.map((c)=>{
+                    return( 
+                        <div className="col-lg-2 col-md-3 col-6 position-relative">
+                            <div key={c.id} className="text-center card-category w-100 my-2 my-md-1">
+                                <img src={c.image} className="w-100 "/>
+                                <div className="category-label fw-semibold position-absolute bottom-0 w-100">
+                                    {c.description}
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+                
+            </div>
+    
+        </div>
         
     )
 }
--- frontend/src/component/HomePage.js
+++ frontend/src/component/HomePage.js
@@ -1,12 +1,14 @@
 import Navigation from "./Navigation";
 import BannerPromotion from "./Banner";
 import { Provider } from "./MyContext";
+import CategoryCard from "./CategoryCard";
 
 const HomePage = ()=>{
     return(
             <Provider>
                 <Navigation/>
                 <BannerPromotion/>
+                <CategoryCard/>
             </Provider>
     )
 }
