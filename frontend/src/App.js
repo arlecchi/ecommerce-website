8d943b7689e9fc37885529f0a7d50dd7e562bc22
ikantapilele <leilani4703@gmail.com>
2025-03-20 12:44:39 +0700

    updates done by lele

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Navigation from "./component/Navigation";
 import './App.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUp from "./component/SignUp";
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/product/:id" element={<ProductDetail />} />
                 <Route path="/cart" element={<CartPage />} />
+                <Route path="/sign-up" element={<SignUp/>}/>
             </Routes>
         </Router>
     );
