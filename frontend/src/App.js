d016fa2def6babef4fb7e615371cf3a55fabd5c7
arlecchi <hasyaamalya@gmail.com>
2025-06-04 10:57:25 +0700

    plssss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./component/Login";
 
 // Import your Context Provider here
-import { Provider } from "./mycontext";  // Adjust the path if needed
+import { Provider } from "./component/MyContext";  // Adjust the path if needed
 
 const App = () => {
     return (
